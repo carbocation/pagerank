61b70973852d38450e3ee6dd239419244f43161b
James Pirruccello <james@carbocation.com>
2017-10-22 17:35:42 -0400

    Initial commit creating the Pagerank mechanisms and a couple tests.

---
+++ pagerank.go
@@ -0,0 +1,124 @@
+// Package pagerank computes PageRank on up to MaxUInt32 (4,294,967,295) items via graph traversal.
+//
+// The demonstration that this method is effective in calculating PageRank
+// comes via http://arxiv.org/pdf/1006.2880.pdf
+//
+// The key insight is that simple graph iteration and counting can approximate PageRank,
+// and can also -- if the graph is stored -- be used to inexpensively recalculate PageRank
+// when edges are added to or removed from the graph.
+//
+// This package does not ultimately store the entire graph traversal history, and therefore
+// it does not assist with recalculating PageRank when edges are removed from the graph.
+// However, it is designed to make the addition of edges easy and inexpensive, since it stores
+// traversal counts from prior runs. The choice to store traversal counts (rather than the full
+// traversal history) is the reason that edge removal is not facilitated.
+package pagerank
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+type Node interface {
+	ID() uint64         // Node identifier
+	Traverse()          // Increment traversal counter for this node and mark it changed
+	Traversals() uint64 // This node has been traversed N times
+	IsStarter() bool    // Should we start iterations from this node?
+	//IsChanged() bool    // Was this node changed by traversals?
+}
+
+type graph struct {
+	nodeMap    map[Node]int
+	nodes      []Node
+	edges      [][]Node
+	traversals uint64
+
+	// Caches and other unexported values
+	randomSource    *rand.Rand
+	jumpProbability float32
+	calculated      bool
+}
+
+func NewGraph(seed int64) *graph {
+	return &graph{
+		randomSource: rand.New(rand.NewSource(seed)),
+		nodeMap:      make(map[Node]int),
+		nodes:        make([]Node, 0),
+		edges:        make([][]Node, 0),
+	}
+}
+
+func (g *graph) AddEdge(nodeFrom, nodeTo Node) {
+	if _, exists := g.nodeMap[nodeFrom]; !exists {
+		g.nodes = append(g.nodes, nodeFrom)
+		g.edges = append(g.edges, make([]Node, 0))
+		g.nodeMap[nodeFrom] = len(g.nodes) - 1
+	}
+
+	if _, exists := g.nodeMap[nodeTo]; !exists {
+		g.nodes = append(g.nodes, nodeTo)
+		g.edges = append(g.edges, make([]Node, 0))
+		g.nodeMap[nodeTo] = len(g.nodes) - 1
+	}
+
+	g.edges[g.nodeMap[nodeFrom]] = append(g.edges[g.nodeMap[nodeFrom]], nodeTo)
+}
+
+func (g *graph) Pagerank(node Node, normalized bool) (float32, error) {
+	if !g.calculated {
+		return 0, fmt.Errorf("Pagerank graph has not yet been calculated")
+	}
+
+	if g.traversals == 0 {
+		for _, node := range g.nodes {
+			g.traversals += node.Traversals()
+		}
+	}
+
+	if normalized {
+		return float32(float64(node.Traversals()) / float64(g.traversals)), nil
+	}
+
+	return float32(float64(len(g.edges)) * float64(node.Traversals()) / float64(g.traversals)), nil
+}
+
+func (g *graph) outlinks(node Node) []Node {
+	return g.edges[g.nodeMap[node]]
+}
+
+func (g *graph) traverseFrom(node Node) {
+	node.Traverse()
+
+	// Terminate the traversal with probability 1/g.jumpProbability
+	if x := g.randomSource.Float32(); x < g.jumpProbability {
+		return
+	}
+
+	outlinks := g.outlinks(node)
+
+	// Terminate the traversal if the node has no outgoing links
+	if len(outlinks) < 1 {
+		return
+	}
+
+	// Continue the traversal from a randomly chosen outgoing link
+	g.traverseFrom(outlinks[g.randomSource.Intn(len(outlinks))])
+}
+
+// Calculate runs the Pagerank computation on your graph in-place.
+func (g *graph) Calculate(JumpProbability float32, RoundsPerNode int) {
+	g.jumpProbability = JumpProbability
+
+	for _, node := range g.nodes {
+		// Only start at starter nodes
+		if !node.IsStarter() {
+			continue
+		}
+
+		for round := 0; round < RoundsPerNode; round++ {
+			g.traverseFrom(node)
+		}
+	}
+
+	g.calculated = true
+}
---
+++ pagerank_test.go
@@ -0,0 +1,148 @@
+package pagerank
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+	"testing"
+)
+
+// Satisfies the pagerank.Node interface
+type nodeExample struct {
+	name       string
+	id         uint64
+	traversals uint64
+	nonstarter bool
+	changed    bool
+}
+
+func (n *nodeExample) ID() uint64 {
+	return n.id
+}
+
+func (n *nodeExample) Traversals() uint64 {
+	return n.traversals
+}
+
+func (n *nodeExample) IsStarter() bool {
+	return !n.nonstarter
+}
+
+func (n *nodeExample) Traverse() {
+	n.traversals++
+	n.changed = true
+}
+
+type adjacency struct {
+	Left  string
+	Right string
+}
+
+var wikiStructure []adjacency = []adjacency{
+	{"b", "c"},
+	{"c", "b"},
+	{"d", "a"},
+	{"d", "b"},
+	{"e", "d"},
+	{"e", "b"},
+	{"e", "f"},
+	{"f", "b"},
+	{"f", "e"},
+	{"v", "e"},
+	{"v", "b"},
+	{"w", "e"},
+	{"w", "b"},
+	{"x", "e"},
+	{"x", "b"},
+	{"y", "e"},
+	{"z", "e"},
+}
+
+func TestPagerank(t *testing.T) {
+	g := buildNewGraph(wikiStructure)
+
+	// Compute
+	g.Calculate(0.15, 500)
+
+	for _, nodeMasked := range g.nodes {
+		node := nodeMasked.(*nodeExample)
+		pr, err := g.Pagerank(node, true)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Logf("%s (%d): %d traversals, PR %.4f", node.name, node.ID(), node.Traversals(), pr)
+	}
+	fmt.Printf("%+v", g)
+}
+
+func TestPagerankParallel(t *testing.T) {
+	graphs := make([]*graph, runtime.NumCPU(), runtime.NumCPU())
+	for i := range graphs {
+		graphs[i] = buildNewGraph(wikiStructure)
+	}
+
+	// Compute
+	wg := sync.WaitGroup{}
+	for _, g := range graphs {
+		wg.Add(1)
+		go func(g *graph) {
+			g.Calculate(0.15, 1000000)
+			wg.Done()
+		}(g)
+	}
+	wg.Wait()
+
+	graph := graphs[0]
+
+	var masterNode, derivedNode *nodeExample
+	for i, g := range graphs {
+		if i == 0 {
+			continue
+		}
+
+		for j, node := range g.nodes {
+			masterNode = graph.nodes[j].(*nodeExample)
+			derivedNode = node.(*nodeExample)
+			masterNode.traversals += derivedNode.traversals
+			if derivedNode.changed {
+				masterNode.changed = true
+			}
+		}
+	}
+
+	for _, nodeMasked := range graph.nodes {
+		node := nodeMasked.(*nodeExample)
+		pr, err := graph.Pagerank(node, true)
+		if err != nil {
+			t.Error(err)
+		}
+		t.Logf("%s (%d): %d traversals, PR %.4f", node.name, node.ID(), node.Traversals(), pr)
+	}
+	fmt.Printf("%+v", graph)
+}
+
+func buildNewGraph(fromSource []adjacency) *graph {
+	g := NewGraph(31337)
+
+	// Map our nodes
+	nodes := make([]*nodeExample, 0)
+	nodeMap := make(map[string]int)
+	letters := make([]string, 2, 2)
+	var i uint64
+	for _, link := range fromSource {
+		letters[0] = link.Left
+		letters[1] = link.Right
+		for _, letter := range letters {
+			if _, exists := nodeMap[letter]; exists {
+				continue
+			}
+			nodes = append(nodes, &nodeExample{id: i, name: letter})
+			nodeMap[letter] = len(nodes) - 1
+			i++
+		}
+		//append(nodes[nodeMap[link.Left]].)
+		g.AddEdge(nodes[nodeMap[link.Left]], nodes[nodeMap[link.Right]])
+	}
+
+	return g
+}
